9c28acb137ed83cbafdc0afbfecbb0a6dad05a62
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-09-23 19:59:05 +0530

    A small code that could be used as Do While Loop.

---
+++ 12 - Loops/doWhile.py
@@ -0,0 +1,24 @@
+i = 0
+
+while True:
+    print(i)
+    i = i + 1
+    if i > 3:
+        break
+
+
+
+# for and while is used when you need to check the termination condition and then use it.
+# do while is used when you need to use it and then check the termination condition.
+# The above program is a small code to make you understand how to do it.
+
+'''
+Print [ 0 , 3]
+works as do while - Since there's no do while in Python - You could use it like this.
+It uses i and then increment it and then compare it with termination condition.
+ and prints - 
+ 0
+ 1
+ 2
+ 3
+'''
