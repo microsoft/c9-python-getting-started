e3e117d8c23bbfe5ee33354bd69782f83601c360
任成 <rencheng11@icloud.com>
2019-09-24 09:40:54 +0800

    Update coding_challenge.py

--- 2 - Print/coding_challenge.py
+++ 2 - Print/coding_challenge.py
@@ -5,7 +5,7 @@
 print('Why won't this line of code print')
 
 # print the message
-prnit('This line fails too!')
+print('This line fails too!')
 
 # print the message
 print "I think I know how to fix this one"
