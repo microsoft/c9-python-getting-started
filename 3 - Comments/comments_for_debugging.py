1f877f6c048bc7e76ffcf086a7a239a9860626c8
smilegodly <35276477+smilegodly@users.noreply.github.com>
2019-09-26 13:27:39 -0400

    Update comments_for_debugging.py

--- 3 - Comments/comments_for_debugging.py
+++ 3 - Comments/comments_for_debugging.py
@@ -1,6 +1,8 @@
-#This single-quote string contains an apostrophe which will produce an invalid syntax error.
-print('It's a small world after all') <-- syntax error
-#To fix this either use an escape sequence character or double quotes
 print('Hello world')
-print('It\'s a small world after all') #escape sequence char
-print("It's a small world after all") #double quotes
+
+#The below single-quote string contains an apostrophe which will produce an invalid syntax error.
+print('It's a small world after all') <-- syntax error
+#To fix this, you can use either an escape sequence character or double quotes.
+
+print('It\'s a small world after all') #escape sequence char fix
+print("It's a small world after all") #double quotes fix
