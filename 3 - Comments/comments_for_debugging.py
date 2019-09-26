8e3d662c2f32e9edb0e99c75f01d8cc2096976de
smilegodly <35276477+smilegodly@users.noreply.github.com>
2019-09-26 13:25:13 -0400

    Gave more context to comments_for_debugging.py
    Explanation / Context of the error , and shows 2 ways to fix it.

--- 3 - Comments/comments_for_debugging.py
+++ 3 - Comments/comments_for_debugging.py
@@ -1,2 +1,6 @@
+#This single-quote string contains an apostrophe which will produce an invalid syntax error.
+print('It's a small world after all') <-- syntax error
+#To fix this either use an escape sequence character or double quotes
 print('Hello world')
-print('It's a small world after all')
+print('It\'s a small world after all') #escape sequence char
+print("It's a small world after all") #double quotes
