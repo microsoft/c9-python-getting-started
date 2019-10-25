0ac7f340d0fa3a7a3e3e4195b830855ffab5b1b5
RIZWAN AHMAD <rizwansoaib@gmail.com>
2019-10-25 16:57:10 +0530

    Update lists.py
    add negative indexing

--- 11 - Collections/lists.py
+++ 11 - Collections/lists.py
@@ -4,7 +4,5 @@ scores.append(98)
 scores.append(99)
 print(names)
 print(scores)
-
 #print element from right use - (negative) e.g. print last first element as -1 : names[-1]=Susan
-
 print(names[-1])
