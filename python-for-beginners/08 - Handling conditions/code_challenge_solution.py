bf3b492164ce62e22d7b87c261379ce868df9a18
jesusleejy <jesusleejy@naver.com>
2020-06-06 19:10:43 +0900

    [UPDATE] fixed indent of pfb-8/code_challenge_solution.py

--- python-for-beginners/08 - Handling conditions/code_challenge_solution.py
+++ python-for-beginners/08 - Handling conditions/code_challenge_solution.py
@@ -6,8 +6,8 @@ price = input('how much did you pay? ')
 price = float(price)
 
 if price >= 1.00:
-	tax = .07
-	print('Tax rate is: ' + str(tax))
+    tax = .07
+    print('Tax rate is: ' + str(tax))
 else:
     tax = 0
     print('Tax rate is: ' + str(tax))
