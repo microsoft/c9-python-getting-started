8f38ff2646dbfddb7a929df5b80b36b4372edeb0
jesusleejy <jesusleejy@naver.com>
2020-06-06 19:34:20 +0900

    [UPDATE] fixed indent of pfb-10/boolean_variables.py

--- python-for-beginners/10 - Complex conditon checks/boolean_variables.py
+++ python-for-beginners/10 - Complex conditon checks/boolean_variables.py
@@ -4,12 +4,12 @@ lowest_grade = float(input('What was your lowest grade? '))
 
 # Boolean variables allow you to remember a True/False value
 if gpa >= .85 and lowest_grade >= .70:
-	honour_roll = True
+    honour_roll = True
 else:
-	honour_roll = False
+    honour_roll = False
 
 # Somewhere later in your code if you need to check if students is 
 # on honour roll, all I need to do is check the boolean variable
 # I set earlier in my code
 if honour_roll:
-	print('You made honour roll')
+    print('You made honour roll')
