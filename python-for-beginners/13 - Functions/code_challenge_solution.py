4968f5024a033097d6921001fb4f25ad74992ec0
ChaeyeonSeo <48763622+ChaeyeonSeo@users.noreply.github.com>
2020-06-07 21:00:39 +0900

    [UPDATE]update comment pfb-13-code_challenge_solution.py

--- python-for-beginners/13 - Functions/code_challenge_solution.py
+++ python-for-beginners/13 - Functions/code_challenge_solution.py
@@ -5,6 +5,7 @@
 # operation: the word 'add' or 'subtract'
 # the function should return the result of the two numbers added or subtracted
 # based on the value passed in for the operator
+#
 def calculator(first_number, second_number, operation):
     if operation.upper() == 'ADD':
         return(float(first_number) + float(second_number))
