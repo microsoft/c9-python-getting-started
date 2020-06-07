3fc256c78c35c1cbcfa6a6da2d17f224f12a96a7
DorikA <63792758+Dorika0203@users.noreply.github.com>
2020-06-08 03:02:48 +0900

    [RESET] Change bad.py to original one

--- more-python-for-beginners/01 - Formatting and linting/bad.py
+++ more-python-for-beginners/01 - Formatting and linting/bad.py
@@ -2,10 +2,10 @@ x = 12
 if x == 24:
     print('Is valid')
 else:
-    print("Not valid")
+ print("Not valid")
 
 def helper(name='sample'):
     pass
 
 def another(name = 'sample'):
-    pass
+ pass
