c9e2c278cdbf436a8bc6c8526d67d343658c7357
ChaeyeonSeo <48763622+ChaeyeonSeo@users.noreply.github.com>
2020-06-07 21:04:18 +0900

    [UPDATE]update pfb-13-code_challenge_solution.py

--- python-for-beginners/13 - Functions/code_challenge_solution.py
+++ python-for-beginners/13 - Functions/code_challenge_solution.py
@@ -13,15 +13,13 @@ def calculator(first_number, second_number, operation):
         return(float(first_number) - float(second_number))
     else:
         return('Invalid operation please specify ADD or SUBTRACT')
-
 # Test your function with the values 6,4, add 
-# Should return 10#
+# Should return 10
+#
 print('Adding 6 + 4 = ' + str(calculator(6,4,'add')))
-
 # Test your function with the values 6,4, subtract 
 # Should return 2
 print('Subtracting 6 - 4 = ' + str(calculator(6,4,'subtract')))
-
 # Test your function with the values 6,4, divide 
 # Should return some sort of error message
 print('Dividing 6 / 4 = ' + str(calculator(6,4,'divide')))
