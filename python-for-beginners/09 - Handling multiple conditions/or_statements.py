931bef85722d888faadfcef53efb51c8db5ec378
jesusleejy <jesusleejy@naver.com>
2020-06-08 17:08:17 +0900

    [UPDATE] fixed indent of pfb-9/or_statements-1.py

--- python-for-beginners/09 - Handling multiple conditions/or_statements.py
+++ python-for-beginners/09 - Handling multiple conditions/or_statements.py
@@ -1,8 +1,8 @@
 province = input("What province do you live in? ")
 tax = 0
 if province == 'Alberta' \
-    or province == 'Nunavut':
-	tax = 0.05
+        or province == 'Nunavut':
+    tax = 0.05
 elif province == 'Ontario':
     tax = 0.13
 else:
