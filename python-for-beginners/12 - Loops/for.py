6272b47a037229644b48b5c0971cdd87247c8c24
Vicky-Raghuwanshi07 <74697810+Vicky-Raghuwanshi07@users.noreply.github.com>
2021-10-05 18:42:18 +0530

    for_loop.py
    This one will show you how for loop works on a list/set/ dict

--- python-for-beginners/12 - Loops/for.py
+++ python-for-beginners/12 - Loops/for.py
@@ -1,2 +1,10 @@
-for name in ['Christopher', 'Susan']:
-	print(name)
+name = ['Christopher', 'Susan']  ## list of names
+
+for nm in name:   ## This will iterate(in simple words it will fetch the values) the name list one by one till the last element/values reached
+	print(nm)
+
+OUTPUT :
+	Christopher
+	Susan
+	
+# For better understanding just take a list of numbers( i.e, [0,1,2,3,4,5,6,7,8,9]) and iterate this list using a for loop
